c75fd0bb1843c8ab3fb5806bb912b6c955164759
Brice <bricetangmouo@gmail.com>
2025-07-20 12:39:11 -0400

    all frontEnd is done

--- client/index.js
+++ client/index.js
@@ -41,4 +41,51 @@ function onPageLoad(){
     })
 }
 
+function dummies_transformed(){
+    var_dummies_transformed = {
+        "province": document.getElementById('uiProvince'),
+        "city": document.getElementById('uiCity'),
+        "home_type": document.getElementById('uiHomeType'),
+        "is_smoking": document.getElementById('uiSmokingPermission')
+    }
+    return var_dummies_transformed
+}
+
+function quantifies_variable(){
+    quantified_variables = {
+        "beds": parseInt(document.getElementById('ui_beds_nbr')),
+        "baths": arseFloat(document.getElementById('ui_baths_nbr')),
+        "sq_feet": parseInt(document.getElementById('ui_square_feet_nbr')),
+        "cats": convert_cats_dogs_to_number(document.getElementById('cats')),
+        "dogs": convert_cats_dogs_to_number(document.getElementById('dogs')),
+        'nbr_beds': parseInt(document.getElementById('ui_beds_nbr'))
+    }
+    return quantifies_variables
+}
+
+function convert_cats_dogs_to_number(value) {
+    if (value === 'True') return 1;
+    if (value === 'False') return 0;
+    console.log('Erreur de conversion:', value);
+    return -1;
+}
+
+function on_click_estimate_price() {
+    var url = "http://127.0.0.1:5000/predict_home_price";
+    $.post(url, {
+        "province": document.getElementById('uiProvince').value,
+        "city": document.getElementById('uiCity').value,
+        "home_type": document.getElementById('uiHomeType').value,
+        "smoking_permission": document.getElementById('uiSmokingPermission').value,
+        "beds": parseInt(document.getElementById('ui_beds_nbr').value),
+        "baths": parseFloat(document.getElementById('ui_baths_nbr').value),
+        "sq_feet": parseInt(document.getElementById('ui_square_feet_nbr').value),
+        "cats": convert_cats_dogs_to_number(document.getElementById('ui_Is_cats_allowed').value),
+        "dogs": convert_cats_dogs_to_number(document.getElementById('ui_is_dogs_allowed').value),
+        "nb_beds": parseInt(document.getElementById('ui_beds_nbr').value)
+    }, function(data, status) {
+        console.log("Prediction response:", data);
+        document.getElementById('uiEstimatedPrice').innerHTML = "<b>Prix estimé : $" + data.estimated_price.toLocaleString() + "</b>";
+    });
+}
 window.onload = onPageLoad
